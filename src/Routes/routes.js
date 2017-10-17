5f1a841734ef3272b7cb7a07eb240e28610b212a
Jonas <jonasmohr@hotmail.com>
2017-10-17 15:19:18 +0200

    Index works

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var mongodb = require('mongodb').MongoClient;
+var url = 'mongodb://localhost/vilduhelst';
+var globalResults;
+
 
 router
     .get('/', function (req, res) {
-        res.send('Welcome to home page');
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('dilemmas');
+            collection.aggregate([{
+                $sample: {
+                    size: 1
+                }
+            }]).toArray(function (err, results) {
+                globalResults = results[0];
+                res.render('index', {
+                    dilemma: results[0]
+                });
+                db.close();
+            });
+        });
     });
 
 router
@@ -12,5 +30,32 @@ router
         res.send('Welcome to about page');
     });
 
+
+router
+    .post('/', function (req, res) {
+        var id = globalResults._id;
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('dilemmas');
+
+            if (req.body.buttoncolor === 'red') {
+                collection.updateOne({
+                    '_id': id
+                }, {
+                    $inc: {
+                        red_dilemma_votes: +1
+                    }
+                });
+            } else if (req.body.buttoncolor === 'blue') {
+                collection.updateOne({
+                    '_id': id
+                }, {
+                    $inc: {
+                        blue_dilemma_votes: +1
+                    }
+                });
+            }
+            db.close();
+        });
+    });
 
 module.exports = router;
